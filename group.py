c3c179c14ba088572cb56f639e74bbd8fd245674
Paddleve117 <123141321+Paddleve117@users.noreply.github.com>
2025-10-13 18:24:28 +0100

    feat: Add fuc about max ave age

--- group.py
+++ group.py
@@ -1,8 +1,6 @@
 """An example of how to represent a group of acquaintances in Python."""
 
 # Your code to go here...
-
-my_group =
 group = {
     "Jill": {
         "age": 26,
@@ -35,3 +33,54 @@ group = {
         }
     }
 }
+
+def _relations_obj(person):
+    return person.get("relations") or {}
+
+def _relation_count(person):
+    return len(_relations_obj(person))
+
+def _has_friend(person):
+    # for r in _relations_obj(person).values():
+    #     if r == "friend":
+    #         return True
+    # return False
+    return len(r for r in _relations_obj(person).values() if r == "friend") > 0
+
+def max_age(group_dict):
+    return max(p.get("age", 0) for p in group_dict.values())
+
+def average_age_relations(group_dict):
+    total_age = 0
+    count = 0
+    for p in group_dict.values():
+        if len(p.get("relations")) > 0:
+            total_age += p.get("age", 0)
+            count += 1
+
+    if total_age == 0:
+        return 0
+    return total_age / count
+
+def max_age_with_relation(group_dict):
+    return max((p.get("age", 0) for p in group_dict.values() if _relation_count(p) > 0), default=0)
+
+def average_age_with_friend(group_dict):
+    total_age = 0
+    count = 0
+    for p in group_dict.values():
+        if len(p.get("relations")) > 0 and _has_friend(p):
+            total_age += p.get("age", 0)
+            count += 1
+
+    if total_age == 0:
+        return 0
+    return total_age / count
+
+def max_age_with_friend(group_dict):
+    return max((p.get("age", 0) for p in group_dict.values() if _has_friend(p)), default=0)
+
+if __name__ == "__main__":
+    print("maximum age:", max_age(group))
+    print("maximum age of relations:", max_age_with_relation(group))
+    print("maximum age (has at least one friend):", max_age_with_friend(group))
