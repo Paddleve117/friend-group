d37a13fa31038ff8080a87ed11552dca4bb272df
Paddleve117 <123141321+Paddleve117@users.noreply.github.com>
2025-10-13 09:27:43 +0100

    feat: friends data structure

--- group.py
+++ group.py
@@ -2,4 +2,38 @@
 
 # Your code to go here...
 
-my_group =
+my_group = [{}, {}, {}, {}]  # Replace with actual dictionaries representing acquaintances
+my_group[0] = {
+    "name": "Jill",
+    "age": 26,
+    "job": "biologist",
+    "friends": {"friend" : ["Zalika"], "partner" : ["John"]}
+}
+my_group[1] = {
+    "name": "Zalika",
+    "age": 28,
+    "job": "artist",
+    "friends": {
+        "friend": ["Jill"],
+        "landlord": ["Nash"]
+    }
+}
+my_group[2] = {
+    "name": "John",
+    "age": 27,
+    "job": "writer",
+    "relations": {
+        "partner": ["Jill"],
+        "cousin": ["Nash"]
+    }
+}
+my_group[3] = {
+    "name": "Nash",
+        "age": 34,
+        "job": "chef",
+        "relations": {
+            "cousin": ["John"],
+            "tenant": ["Zalika"]
+        }
+}
+
